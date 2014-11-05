6ce806acc21681edef67fe11d14e6c68bf493a5b
Patrick Shampine <pshampine@gunnjerkens.com>
2014-11-05 13:24:16 -0800

    Fixed js to create new fields dynamically

--- public/js/app.js
+++ public/js/app.js
@@ -1988,30 +1988,30 @@ $(document).ready(function() {
 $(document).ready(function() {
 
   $('.new-item').on('click', function() {
-    var rowID, rowHTML;
+    var rowsArr, rowID, rowHTML;
+    rowsArr = [];
 
-    rowID = $('tbody').children().length;
+    $('tbody').children().each(function() {
+      rowsArr.push($(this).data('id'));
+    });
+    rowID = rowsArr.slice(-1).pop() + 1;
 
-    rowHTML = '<tr dataid="' + rowID + '">';
-
-    console.log(fields);
+    rowHTML = '<tr data-id="' + rowID + '">';
+    rowHTML += '<input type="hidden" name="' + rowID + '[id]" value="' + rowID + '">';
 
     for(var i = 0; i < fields.length; i++) {
       rowHTML += '<td>';
-      if(fields[i].field_name === 'text' || fields[i].field_name === 'checkbox') {
+      if(fields[i].field_type === 'text' || fields[i].field_type === 'checkbox') {
         rowHTML += '<input type="' + fields[i].field_type + '" name="' + rowID + '[' + fields[i].field_name + ']">';
-      } else if(fields[i].field_name === 'select') {
+      } else if(fields[i].field_type === 'select') {
         rowHTML += '<select name="' + rowID + '[' + fields[i].field_name + ']">';
         for(var j = 0; j < fields[i].field_values.length; j ++) {
           rowHTML += '<option value="' + fields[i].field_values[j].value + '">' + fields[i].field_values[j].label + '</option>';
         }
         rowHTML += '</select>';
-      } else {
-        console.log('nope');
       }
       rowHTML += '</td>';
     }
-
     rowHTML += '</tr>';
 
     $('tbody').append(rowHTML);

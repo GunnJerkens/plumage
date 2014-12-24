37328f0df5678efee4122b649092a7eb74267221
Patrick Shampine <pshampine@gunnjerkens.com>
2014-12-24 11:22:41 -0800

    Added sample env file

---
+++ sample.env.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+  'HOSTNAME'       => 'localhost',
+  'DEBUG'          => false,
+  'TIMEZONE'       => 'UTC',
+  'ENCRYPTION_KEY' => 'YIMYrt2kC1sCoMTea4S2JeeYdH0JmVKr',
+
+  // DATABASE
+  'DB_NAME'        => 'plumage_dev',
+  'DB_HOST'        => 'localhost',
+  'DB_USER'        => 'root',
+  'DB_PASS'        => '',
+  'DB_CHARSET'     => 'utf8',
+  'DB_COLLATION'   => 'utf8_unicode_ci',
+  'DB_PREFIX'      => '',
+
+  // SERVICES
+  'MAILGUN_DOMAIN' => '',
+  'MAILGUN_SECRET' => '',
+];

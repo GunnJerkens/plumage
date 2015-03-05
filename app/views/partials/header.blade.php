1198dc8297e97602a4187dd879c89671ad8a6668
Patrick Shampine <pshampine@gunnjerkens.com>
2015-03-04 17:26:43 -0800

    Meant to be noindex, nofollow

--- app/views/partials/header.blade.php
+++ app/views/partials/header.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="robots" content="index, follow">
+    <meta name="robots" content="noindex, nofollow">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     @yield('title')
     <link href="/css/app.css" rel="stylesheet">

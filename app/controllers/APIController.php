12b08888bb63dba85619a936456669cefd645097
Patrick Shampine <pshampine@gunnjerkens.com>
2015-10-19 10:41:16 -0700

    Moved api missing bttf quotes into land, added delorean

--- app/controllers/APIController.php
+++ app/controllers/APIController.php
@@ -84,22 +84,8 @@ class APIController extends Controller
    */
   public function getAPIMissing()
   {
-    $quotes = [
-      'There\'s that word again. "Heavy." Why are things so heavy in the future? Is there a problem with the Earth\'s gravitational pull?',
-      'Roads? Where we\'re going, we don\'t need roads.',
-      'Great Scott!',
-      'Ronald Reagan? The actor?',
-      'You want a Pepsi, pal, you\'re gonna pay for it.',
-      'This is heavy.',
-      'No, no, no, no, no, this sucker\'s electrical, but I need a nuclear reaction to generate the 1.21 gigawatts of electricity I need.',
-      'What\'s a rerun?',
-      'What-what the hell is a gigawatt?',
-      'If you put your mind to it, you can accomplish anything.',
-      'Weight has nothing to do with it.',
-      'Lorraine. My density has brought me to you.',
-      'Yes. Yes. I\'m George. George McFly. I\'m your density. I mean, your destiny.',
-    ];
-    $key = array_rand($quotes);
+    $quotes = Lang::get('bttf');
+    $key    = array_rand($quotes);
     return Response::json($quotes[$key], 404);
   }
 

57a5de0601c4744730347243eadd9c988c9dbf39
Alex Medina <amedina@gunnjerkens.com>
2016-03-11 11:37:01 -0800

    Closes #40 -- Fire warnings on project delete

--- app/views/layouts/dashboard.blade.php
+++ app/views/layouts/dashboard.blade.php
@@ -28,7 +28,26 @@
                     <td>
                       <a href="{{ '/project/'.$project->id }}" title="{{ $project->name }}">{{ $project->name }}</a>
                       @if($project->is_owner)
-                        <form role="form" action="{{ '/project/'.$project->id.'/delete' }}" method="post"><input type="hidden" name="_token" value="{{ csrf_token() }}"><button class="btn-delete"><i class="fa fa-times-circle"></i></button></form>
+                        <form role="form" action="{{ '/project/'.$project->id.'/delete' }}" method="post">
+                          <button type="button" class="btn-delete" data-toggle="modal" data-target="#deleteModal{{ $project->id }}"><i class="fa fa-times-circle"></i></button>
+                          <div id="deleteModal{{ $project->id }}" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="deleteModal">
+                            <div class="modal-dialog">
+                              <div class="modal-content">
+                                <div class="modal-header">
+                                  <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                  <h4 class="modal-title">Confirm Delete</h4>
+                                </div>
+                                <div class="modal-body">
+                                  <p>Are you sure you want to delete <strong>{{ $project->name}}</strong>?</p>
+                                </div>
+                                <div class="modal-footer">
+                                 <button type="button" class="btn" data-dismiss="modal">Cancel</button>
+                                 <input type="hidden" name="_token" value="{{ csrf_token() }}"><button class=" btn btn-danger">Delete</button>
+                               </div>
+                              </div>
+                            </div>
+                          </div>
+                        </form>
                       @endif
                     </td>
                   </tr>

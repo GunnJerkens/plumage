3d82170aaebf436aa52f15b7bbaeb1a49d5ee551
Alex Medina <amedina@gunnjerkens.com>
2016-03-10 16:24:32 -0800

    adds fixed header markup

--- app/views/layouts/type.blade.php
+++ app/views/layouts/type.blade.php
@@ -26,7 +26,7 @@
             <div class="row">
               <div class="col-sm-12">
                 <div class="form-inner-wrapper">
-                  <table class="table table-striped table-bordered">
+                  <table class="table table-striped table-bordered table-fixed-header hidden-xs" table-id="type-table">
                     <thead>
                       <tr>
                         @if($user->is_admin || $project->is_owner || $access->can_delete)
@@ -37,44 +37,58 @@
                         @endforeach
                       </tr>
                     </thead>
-                    <tbody>
-                    @if(sizeof($items) > 0)
-                      @foreach($items as $item)
-                        <tr data-id="{{ $item->id }}">
-                          <input type="hidden" name="{{ $item->id }}[id]" value="{{ $item->id }}">
+                  </table>
+                  <div class="table-fixed-header-wrap">
+                    <table id="type-table" class="table table-striped table-bordered">
+                      <thead>
+                        <tr>
                           @if($user->is_admin || $project->is_owner || $access->can_delete)
-                            <td><a href class="js-delete-type btn-delete" data-id="{{ $item->id }}"><i class="fa fa-times-circle"></i></a>
+                            <td>Delete</td>
                           @endif
                           @foreach($fields as $field)
-                            @if($field->field_type === 'text')
-                              <td>
-                                <input type="text" name="{{ $item->id }}[{{ $field->field_name }}]" value="{{ $item->{$field->field_name} }}" class="form-control" {{ $user->hasAccess(['manage']) || isset($field->field_editable) && $field->field_editable === 'on' ? '' : ' disabled' }}>
-                              </td>
-                            @elseif($field->field_type === 'checkbox')
-                              <td>
-                                <input type="checkbox" name="{{ $item->id }}[{{ $field->field_name }}]"{{ $item->{$field->field_name} ? ' checked' : '' }} {{ $user->hasAccess(['manage']) || isset($field->field_editable) && $field->field_editable === 'on' ? '' : ' disabled' }} value="1">
-                              </td>
-                            @elseif($field->field_type === 'select')
-                              <td>
-                                <select name="{{ $item->id }}[{{ $field->field_name }}]"{{ $user->hasAccess(['manage']) || isset($field->field_editable) && $field->field_editable === 'on' ? '' : ' disabled' }}>
-                                  @foreach($field->field_values as $option)
-                                    <option value="{{ $option->value }}"{{ $option->value === $item->{$field->field_name} ? ' selected' : '' }}>{{ $option->label }}</option>
-                                  @endforeach
-                                </select>
-                              </td>
-                            @endif
+                            <td>{{ $field->field_name }}</td>
                           @endforeach
                         </tr>
-                      @endforeach
-                    @else
-                      <table id="no-data">
-                        <tr>
-                          <td>No data available.</td>
-                        </tr>
-                      </table>
-                    @endif
-                    </tbody>
-                  </table>
+                      </thead>
+                      <tbody>
+                      @if(sizeof($items) > 0)
+                        @foreach($items as $item)
+                          <tr data-id="{{ $item->id }}">
+                            <input type="hidden" name="{{ $item->id }}[id]" value="{{ $item->id }}">
+                            @if($user->is_admin || $project->is_owner || $access->can_delete)
+                              <td><a href class="js-delete-type btn-delete" data-id="{{ $item->id }}"><i class="fa fa-times-circle"></i></a>
+                            @endif
+                            @foreach($fields as $field)
+                              @if($field->field_type === 'text')
+                                <td>
+                                  <input type="text" name="{{ $item->id }}[{{ $field->field_name }}]" value="{{ $item->{$field->field_name} }}" class="form-control" {{ $user->hasAccess(['manage']) || isset($field->field_editable) && $field->field_editable === 'on' ? '' : ' disabled' }}>
+                                </td>
+                              @elseif($field->field_type === 'checkbox')
+                                <td>
+                                  <input type="checkbox" name="{{ $item->id }}[{{ $field->field_name }}]"{{ $item->{$field->field_name} ? ' checked' : '' }} {{ $user->hasAccess(['manage']) || isset($field->field_editable) && $field->field_editable === 'on' ? '' : ' disabled' }} value="1">
+                                </td>
+                              @elseif($field->field_type === 'select')
+                                <td>
+                                  <select name="{{ $item->id }}[{{ $field->field_name }}]"{{ $user->hasAccess(['manage']) || isset($field->field_editable) && $field->field_editable === 'on' ? '' : ' disabled' }}>
+                                    @foreach($field->field_values as $option)
+                                      <option value="{{ $option->value }}"{{ $option->value === $item->{$field->field_name} ? ' selected' : '' }}>{{ $option->label }}</option>
+                                    @endforeach
+                                  </select>
+                                </td>
+                              @endif
+                            @endforeach
+                          </tr>
+                        @endforeach
+                      @else
+                        <table id="no-data">
+                          <tr>
+                            <td>No data available.</td>
+                          </tr>
+                        </table>
+                      @endif
+                      </tbody>
+                    </table>
+                  </div>
                 </div>
               </div>
             </div>
